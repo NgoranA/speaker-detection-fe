06e69153a5814e606654045edfecedf5898b6295
ngorana <ngoranaristide4@gmail.com>
2024-07-04 17:21:19 +0100

    slight addition to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 	return (
 		<footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
 			<p className="text-xs text-gray-500 dark:text-gray-400">
-				&copy; 2024 Speaker Detection App. All rights reserved.
+				&copy; 2024 Speaker Detection App. All rights reserved. M1 Students
 			</p>
 			<nav className="sm:ml-auto flex gap-4 sm:gap-6">
 				<Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
